9c263f9e4c48b5328a6d4f8dda5f979796d46273
cristiannbaiu <cristiannbaiu@gmail.com>
2015-07-05 16:50:39 +0300

    Added one C# assembly

---
+++ Sources/HelloWorld/CNETClassLib/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNETClassLib
+{
+    public class Class1
+    {
+			private int publicMember;
+
+			public  Class1()
+				{
+			publicMember = 0;
+				}
+
+		public int PublicMember
+		{
+			get
+			{
+				return publicMember;
+			}
+
+			set
+			{
+				publicMember = value;
+			}
+		}
+	}
+}
